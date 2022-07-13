b2131d715d5eeb0dd79cda91a31f80abc69aab13
异想之旅 <danny070601@aliyun.com>
2022-07-13 13:39:11 +0800

    [FIX] Update settings

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from urllib import parse
 
 PATH = pathlib.Path(__file__).parent
 
@@ -22,11 +23,11 @@ if MODE == 'development':
 elif MODE == 'production':
     DEBUG = False
     DB_HOST = os.environ.get('DB_HOST', 'localhost:5432')
-    DB_USERNAME = os.environ.get('DB_USERNAME')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD').replace(':', '::').replace(
-        '@', '@@')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASSWORD = parse.quote_plus(os.environ.get('DB_PASSWORD'))
     DB_DB = os.environ.get('DB_DB')
+    print(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DB}', )
     SQLALCHEMY.update({
         'SQLALCHEMY_DATABASE_URI':
-        f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_DB}',
+        f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DB}',
     })
